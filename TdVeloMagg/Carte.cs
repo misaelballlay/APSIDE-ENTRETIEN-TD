28123ed8956b1f44ad8f8744e2e0da317371240e
misaelballlay <92384205+misaelballlay@users.noreply.github.com>
2021-10-14 10:03:05 +0200

    Add files via upload

---
+++ TdVeloMagg/Carte.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TdVeloMagg
+{
+    class Carte
+    {
+        //Attribut (collection)
+        private List<Station> lesStations;
+
+        //Constructeur
+        public Carte()
+        {
+            this.lesStations = new List<Station>();
+        }
+
+        //Méthodes publiques
+        public void ajouterStation(int id, string nom, string latitude, string longitude, int nbPlacesOccupees, int nbPlacesLibres, int nbPlacesTotales)
+        {
+            Station newStation = new Station(id, nom, latitude, longitude, nbPlacesOccupees, nbPlacesLibres, nbPlacesTotales);
+            lesStations.Add(newStation);
+        }
+
+        public Station getLaStation(int index)
+        {
+            Station laStation;
+
+            laStation = lesStations[index];
+
+            return laStation;
+        }
+
+        public int nbStations()
+        {
+            return lesStations.Count;
+        }
+    }
+}
---
+++ TdVeloMagg/Station.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TdVeloMagg
+{
+    class Station
+    {
+        //Attribut
+        private int id;
+        private string nom;
+        private string latitude;
+        private string longitude;
+        private int nbPlacesOccupees;
+        private int nbPlacesLibres;
+        private int nbPlacesTotales;
+
+        //Constructeur
+        public Station(int id, string nom, string latitude, string longitude, int nbPlacesOccupees, int nbPlacesLibres, int nbPlacesTotales)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.nbPlacesOccupees = nbPlacesOccupees;
+            this.nbPlacesLibres = nbPlacesLibres;
+            this.nbPlacesTotales = nbPlacesTotales;
+        }
+
+        //Accesseurs
+        public int Id { get => id; }
+        public string Nom { get => nom; }
+
+        //Méthodes publiques
+        public string toString()
+        {
+            string res = "";
+
+            res += "Latitude :\t" + this.latitude;
+            res += "\nLongitude :\t" + this.longitude;
+            res += "\nNombre de places occuppées :\t" + this.nbPlacesOccupees;
+            res += "\nNombre de places libres :\t" + this.nbPlacesLibres;
+            res += "\nNombre de places totales :\t" + this.nbPlacesTotales;
+
+            return res;
+        }
+    }
+}
