caab1b5c4927a178e14a777b4cd559b9c10f414a
misaelballlay <92384205+misaelballlay@users.noreply.github.com>
2021-10-14 09:38:49 +0200

    Add files via upload

---
+++ TD VéloMag/Corrigé Luke/TdVeloMagg/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TdVeloMagg
+{
+    public partial class Form1 : Form
+    {
+        //On charge la carte des stations
+        Carte laCarte;
+
+        public Form1()
+        {
+            InitializeComponent();
+            laCarte = Passerelle.getCarte();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //****Code à compléter****
+
+            Carte laCarte = Passerelle.getCarte();
+            //On récupère toutes les stations et on ajoute leur nom à la dataGridView
+            int nbStations = laCarte.nbStations();
+            for(int i = 0; i <= (nbStations-1); i++)
+            {
+                dgvStations_CellContentClick.Rows.Add(laCarte.getLaStation(i).Nom);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //****Code à compléter****
+
+            //On récupère l'index de la ligne de la dataGridView sélectionnée
+            int index = dgvStations_CellContentClick.CurrentCell.RowIndex;
+
+            //On affiche les infos de la station grâce au toString() de la classe station
+            MessageBox.Show(laCarte.getLaStation(index).toString(), laCarte.getLaStation(index).Nom);
+        }
+    }
+}
